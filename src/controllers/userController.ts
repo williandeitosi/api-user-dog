0f203c00d19fc9fb904e3f0b92ec382101a0b866
Willian Giovanini Dei Tosi <104787632+williandeitosi@users.noreply.github.com>
2024-09-04 16:05:45 +0000

    chore: add dogs in user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,8 +6,7 @@ import { emailExists } from "../utils/helpers";
 
 export const createUser = async (req: Request, res: Response) => {
     const data = req.body
-    const { email } = data
-
+    const { email, name, dogNames} = data
 
 
     if (await emailExists(email)) {
@@ -15,7 +14,16 @@ export const createUser = async (req: Request, res: Response) => {
     }
 
 
-    const newUser = await prisma.user.create({ data })
+    const newUser = await prisma.user.create({
+        data: {
+            email,
+            name,
+            dog: {
+                /* como sao varios nomes , isso e para iterara o nome de todos os cachorros */
+                create: dogNames.map((dogName: any) => ({name: dogName}))
+            }
+        }
+    })
 
     if (data) {
         return res.status(201).json({ message: 'user created successfully', user: newUser })
@@ -26,11 +34,13 @@ export const getUser = async (req: Request, res: Response) => {
 
     const id = Number(req.params.id)
 
-    if(!id) {
-        return res.status(404).json({message: 'ID not found'})
+    if (!id) {
+        return res.status(404).json({ message: 'ID not found' })
     }
 
-    const user = await prisma.user.findUnique({ where: { id } })
+    const user = await prisma.user.findUnique({ where: { id }, include: {
+        dog: true
+    } })
 
     if (user) {
         return res.status(200).json(user)
@@ -38,47 +48,55 @@ export const getUser = async (req: Request, res: Response) => {
 }
 
 export const getAllUsers = async (req: Request, res: Response) => {
-   const allUsers =  await prisma.user.findMany()
+    const allUsers = await prisma.user.findMany()
 
-   if(allUsers) {
-    return res.status(200).json(allUsers)
-   }
+    if (allUsers) {
+        return res.status(200).json(allUsers)
+    }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
     const data = req.body
+    const {name, email, dogNames} = data
     const id = Number(req.params.id)
 
-    if(!id) {
-        return res.status(404).json({message: 'ID not found'})
+    if (!id) {
+        return res.status(404).json({ message: 'ID not found' })
     }
 
     const attUser = await prisma.user.update({
-        where: {id},
-        data
+        where: { id },
+        data: {
+            email,
+            name,
+            dog: {
+                create: dogNames.map((dogName: string[]) => ({name: dogName}))
+            }
+        }
+
     })
 
-    if(attUser) {
-        const {id, ...userWithoutId} = attUser
-        return res.status(200).json({message:"Updated successfully", userWithoutId})
+    if (attUser) {
+        const { id, ...userWithoutId } = attUser
+        return res.status(200).json({ message: "Updated successfully", userWithoutId })
     } else {
-        return res.status(500).json({message: 'Unable to update this user'})
+        return res.status(500).json({ message: 'Unable to update this user' })
     }
 
 }
 
-export const deleteUser = async (req:Request, res:Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
     const id = Number(req.params.id)
 
-    if(!id) {
-        return res.status(404).json({message: "ID not found"})
+    if (!id) {
+        return res.status(404).json({ message: "ID not found" })
     }
 
-    const removeUser = await prisma.user.delete({where: {id}})
+    const removeUser = await prisma.user.delete({ where: { id } })
 
-    if(removeUser) {
-        return res.status(200).json({message: "Deleted successfully"})
+    if (removeUser) {
+        return res.status(200).json({ message: "Deleted successfully" })
     } else {
-        return res.status(500).json({message: "Error to deleted this user"})
+        return res.status(500).json({ message: "Error to deleted this user" })
     }
 }
